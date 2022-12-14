2f504fafffd911e04c35363c07766a535320dabf
Mazka Andrii <andriy.mazka@gmail.com>
2022-12-14 21:25:41 +0200

    renderAdd

--- src/index.js
+++ src/index.js
@@ -15,27 +15,66 @@ import axios from 'axios';
 import notiflix from 'notiflix';
 
 const searchBtnRef = document.querySelector('.search-button');
-const searchValue = document.querySelector('#search-form');
+const searchValue = document.querySelector('.main-input');
 const imageList = document.querySelector('.gallery');
 
+const API_KEY = '32012356-0368280beb1a1f5a21315c6c1';
+
+const PageSize = 40;
+let currentPage = 1;
+
 searchBtnRef.addEventListener('click', e => {
+  e.preventDefault();
   getImagesAxios({ query: searchValue.value });
 });
 
-const API_KEY = '32012356-0368280beb1a1f5a21315c6c1';
-const PageSize = 40;
-
-let currentPage = 1;
-let totalPages = undefined;
-
-function getImagesAxios(query) {
-  const urlAPI = `https://pixabay.com/api/?Key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${PageSize}&page=${currentPage}`;
-
-  return axios
+function getImagesAxios({ query }) {
+  const urlAPI = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${PageSize}&page=${currentPage}`;
+  axios
     .get(urlAPI)
     .then(res => res.data)
-    .then(({ articles, totalResults }) => {
-      return { articles, totalResults };
+    .then(({ hits }) => {
+      renderGallery(hits);
     })
-    .catch(error => console.log(error));
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+// рендер галлереи
+function renderGallery(hits) {
+  const imgMarkup = hits.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `
+          <div class="photo-card">
+          <a class="gallery-item" href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" class="photo-card__image"/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes : ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+  </div> </a>
+  `;
+    }
+  );
+
+  imageList.insertAdjacentHTML('beforeend', imgMarkup.join(''));
 }
